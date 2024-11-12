7e7d439d11872bea228aede0ba17052049a4303f
JaoryxHowest <jordan.baveghems@student.howest.be>
2024-11-12 09:30:32 +0100

    Add base client + add client functions

---
+++ index.js
@@ -0,0 +1,37 @@
+require('dotenv').config({ path: 'config/.env' });
+const { Client, Collection, GatewayIntentBits } = require('discord.js');
+const functions = require('./modules/functions');
+
+// Initiate everything
+const client = new Client({ 
+    intents: [
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.GuildMembers,
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildVoiceStates,
+        GatewayIntentBits.MessageContent,
+        GatewayIntentBits.GuildEmojisAndStickers,
+        GatewayIntentBits.GuildModeration,
+        GatewayIntentBits.GuildInvites
+    ]
+});
+
+client.commands = new Collection();
+
+// Link all functions to the client
+const functionKeys = Object.keys(functions);
+for (i = 0; i < functionKeys.length; i++) {
+    client[functionKeys[i]] = functions[functionKeys[i]];
+};
+
+// Handle the unhandled exceptions
+process.on('unhandledRejection', (error, p) => {
+    client.Log('[ERROR]: ' + error);
+})
+.on('uncaughtException', err => {
+    client.Log('[ERROR]: Uncaught exception: ', err);
+});
+
+client.rest.on("rateLimited", client.Log);
+
+client.login(process.env.BOT_TOKEN);
